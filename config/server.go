ff1283ba6661dfd6c1bf34291bef720b51abc91d
Katsunori Kawaguchi <hamakata@gmail.com>
2018-04-08 19:38:43 +0900

    Stop to specify app timezone
    not necessary now

--- config/server.go
+++ config/server.go
@@ -2,24 +2,12 @@ package config
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/hamakn/go_ddd_webapp/src/app/infrastructure/config"
 )
 
 func init() {
-	setTimezone()
 	injectDependencies()
 
 	http.Handle("/", config.NewRouter())
-}
-
-func setTimezone() {
-	loc, err := time.LoadLocation("Asia/Tokyo")
-
-	if err != nil {
-		panic(err)
-	}
-
-	time.Local = loc
 }
--- src/app/test/init_test.go
+++ src/app/test/init_test.go
@@ -1,25 +1,11 @@
 package test
 
 import (
-	"time"
-
 	"github.com/hamakn/go_ddd_webapp/src/app/infrastructure/environments"
 	"github.com/hamakn/go_ddd_webapp/src/app/internal"
 )
 
 func init() {
-	setTimezone()
 	internal.MockEnvironments(&environments.Environments{})
 	injectDependencies()
-}
-
-// same as setTimezone on config/server.go
-func setTimezone() {
-	loc, err := time.LoadLocation("Asia/Tokyo")
-
-	if err != nil {
-		panic(err)
-	}
-
-	time.Local = loc
 }
